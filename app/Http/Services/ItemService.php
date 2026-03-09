87ff48bf5be098e3cbd5e174b4b14e66685513f5
Younes <younes.chahid@etu.univ-tours.fr>
2026-03-09 17:22:42 +0100

    [feature] created the item service

---
+++ app/Http/Services/ItemService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Http\Services\Logs\ItemLoggerService;
+use App\Models\Item;
+
+class ItemService {
+
+
+    private ItemLoggerService $logger;
+
+    public function __construct(ItemLoggerService $logger) {
+        $this->logger = $logger;
+    }
+
+    public function save(Item $item) {
+        $this->logger->log($item);
+        $item->save();
+    }
+
+    /**
+     * Returns the old version of the current item. If it has not been inserted
+     * in the database, returns the same item
+     * @param Item $item
+     * @return Item The database instance of the requested Item, or a new instance if it does not exists
+     */
+    public function getFromItem(Item $item): Item {
+        if (!$item->id) return new Item();
+        return $this->getFromId($item->id);
+    }
+
+    public function getFromId(int $id): Item {
+        return Item::find($id);
+    }
+
+    public function getAll() {
+        return Item::all();
+    }
+
+    public function delete(Item $item) {
+        $this->logger->logDelete($item);
+        $item->delete();
+    }
+
+}
