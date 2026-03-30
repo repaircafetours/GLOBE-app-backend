79257b7c284f8573acb698b41df537c8356595d3
Younes <younes.chahid@etu.univ-tours.fr>
2026-03-30 17:13:37 +0200

    [fix] fixed endpoints unreachable

--- app/Http/Requests/StoreVisitorRequest.php
+++ app/Http/Requests/StoreVisitorRequest.php
@@ -11,7 +11,7 @@ class StoreVisitorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdateVisitorRequest.php
+++ app/Http/Requests/UpdateVisitorRequest.php
@@ -11,7 +11,7 @@ class UpdateVisitorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
